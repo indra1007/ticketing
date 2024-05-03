9bc00ae6076de7ef4fdd4867e439dfa2b0c6c43e
indrawan <indrawan.yunus@gmail.com>
2024-05-03 08:51:47 +0700

    tour added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TourController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/tour', [TourController::class, 'tour']);
